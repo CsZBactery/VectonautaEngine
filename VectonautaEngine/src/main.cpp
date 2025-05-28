374254a99db7b69ae72ac2e735becfc28bf66f31
TeamPizzaMortisUcq <csasia46488@ucq.edu.mx>
2025-05-27 23:52:14 -0600

    Set Up Render Pipeline

--- VectonautaEngine/src/main.cpp
+++ VectonautaEngine/src/main.cpp
@@ -1,31 +1,54 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    // Crear la ventana
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Ejemplo simple de SFML");
+sf::RenderWindow* window;
+sf::CircleShape* circle;
 
-    // Crear un círculo
-    sf::CircleShape circle(100.f); // radio 100
-    circle.setFillColor(sf::Color::Blue);
-    circle.setPosition(200.f, 150.f);
-
-    // Bucle principal
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Cerrar la ventana si el usuario lo indica
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Limpiar, dibujar y mostrar
-        window.clear();
-        window.draw(circle);
-        window.display();
-    }
-
-    return 0;
+void
+init() {
+    window = new sf::RenderWindow(sf::VideoMode(800, 600), "VectonautaEngine");
+    circle = new sf::CircleShape(100.0f);
+    circle->setFillColor(sf::Color::Blue);
+    circle->setPosition(200.f, 150.f);
 }
+
+void
+handleEvents() {
+    sf::Event event;
+    while (window->pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            window->close();
+    }
+}
+
+void
+update() {
+
+}
+
+void
+render() {
+    window->clear();
+    window->draw(*circle);
+    window->display();
+
+}
+
+void
+destroy() {
+    delete window;
+    delete circle;
+}
+
+int
+main() {
+    init();
+
+    while (window->isOpen()) {
+        handleEvents();
+        update();
+        render();
+    }
+    
+        destroy();
+        return 0;
+    }
